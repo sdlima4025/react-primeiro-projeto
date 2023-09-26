7899b5aa9f113cd507dbe47b85dd87790dfda46f
sdlima4025 <sergiolimafg3@gmail.com>
2023-09-26 11:01:04 -0300

    criado componente Square exportado e importado

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
-function Page() {
+import Square from '@/components/Square';
+const Page = () => {
   return (
     <div>
-      <h1>Olá, Mundo!</h1>
+      <h1 className="font-bold text-xl">Olá, Mundo!</h1>
       <h2>dois componentes em uma div</h2>
+      <Square/>
     </div>
+
   );
 }
 
 export default Page;
+
---
+++ src/components/Square.tsx
@@ -0,0 +1,9 @@
+const Square = () => {
+  return (
+    <div className="w-52 h-52 bg-orange-700 text-white">
+        este  componente foi importado 
+    </div>
+  );
+}
+
+export default Square;
