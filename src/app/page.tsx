9400b96499dad947e7230c219ab6232648360045
sdlima4025 <sergiolimafg3@gmail.com>
2023-09-28 10:55:10 -0300

    criação de Childrem

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,35 +1,19 @@
-import { Dog } from "@/components/Dog";
-import { Person } from "@/components/Person";
+import { Card } from "@/components/Card";
+import { Circle } from "@/components/Circle";
 
 const Page = () => {
   return (
-    <div>
-      <h1 className="font-bold text-xl">Olá, Mundo!</h1>
-      <h2>Agrupamento de Componentes</h2>
-
-      <Person
-        name="Elon Musk"
-        avatar="https://classic.exame.com/wp-content/uploads/2021/04/Elon-Musk.jpg"
-        roles={["CEO da Tesla", "CEO da SpaceX"]}
-      />
-
-      <Person
-        name="Jeff Bezos"
-        avatar="https://m.media-amazon.com/images/M/MV5BYTNlOGZhYzgtMmE3OC00Y2NiLWFhNWQtNzg5MjRhNTJhZGVmXkEyXkFqcGdeQXVyNzg5MzIyOA@@._V1_.jpg"
-        roles={["CEO da Amazon", "CEO da Blue Origin"]}
-      />
-      <Person
-        name="Avatar Padrão"
-        roles={["Avatar padrão", "foi utilizado"]}
-      />
-
-      <Dog
-        name="Brad"
-        avatar="https://caocidadao.com.br/wp-content/uploads/2013/08/bulldog-frances_interna.jpg"
-        raca="Buldogue"
-        roles= {['Campeão 2023', 'Pura Raça']}
-      />
-    </div>
+  <div>
+      <h1 className="font-bold text-xl">Children Componentes</h1>
+      
+      <Card>
+        <div>
+          <h3 className="text-3xl font-bold italic">Frase de Efeito</h3>
+          <p className="text-right text-sm">Autor Desconhecido</p>
+          <Circle/>
+        </div>
+      </Card>
+  </div>
   );
 };
 
