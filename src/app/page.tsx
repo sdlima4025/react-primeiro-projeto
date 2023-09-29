a7da5008cd445f14107f31a0589f83ca687ae6c6
sdlima4025 <sergiolimafg3@gmail.com>
2023-09-29 11:39:47 -0300

    Filtando Listas com filter

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,28 @@
 import {peopleList} from '@/data/peopleList';
-import {Card} from "@/components/Card";
-
-
 
 const Page = () => {
   // const list = peopleList.map(person => <li>{person.name}</li>)
+  const chemists = peopleList.filter(person => person.profession === 'cheminist');
   return (
   <div>
       <h1 className="font-bold text-xl">React JS</h1>
    
-      {peopleList.length > 0 && 
+      {/* {peopleList.length > 0 && 
       <ul>
         {peopleList.map(person => 
         <li>{person.id} - {person.name} - {person.profession}</li>)}
       </ul>
+      } */}
+
+      {chemists.length > 0 &&
+        <>
+          <h3>Lista de Quimicos:</h3>
+          <ul>
+            {chemists.map(person =>
+              <li key={person.id}>{person.name}</li>
+              )}
+          </ul>
+        </>
       }
   </div>
   );
--- src/data/peopleList.ts
+++ src/data/peopleList.ts
@@ -4,7 +4,7 @@ export const peopleList: Person[] = [
     {id: 1, name: 'Marcos', profession:'mathematician'},
     {id: 2, name: 'Gilton', profession:'cheminist'},
     {id: 3, name: 'Silvio', profession:'physicist'},
-    {id: 4, name: 'Sérgio', profession:'chenist'},
+    {id: 4, name: 'Sérgio', profession:'cheminist'},
     {id: 5, name: 'Luiz', profession:'astrophysicist'},
-    {id: 6, name: 'Reinaldo', profession:'mathematician'},
+    {id: 6, name: 'Reinaldo', profession:'cheminist'},
 ];
