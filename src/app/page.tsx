23efd46aee0c7fc1853f86c8194d78fa2b764c02
sdlima4025 <sergiolimafg3@gmail.com>
2023-10-02 12:01:27 -0300

    Exercício: Greeting condicional 2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,13 @@
-const Page = () => {
+  /** const Page = () => {
   const fullTime = new Intl.DateTimeFormat('pt-BR',{
     timeStyle:'short',
     hour12: false
   }).format();
 
   const hour = new Date().getHours();
-  /** testando as horas 
+ testando as horas 
    const hour = 15;
-  */
+
   let greeting = '0';
 
   if(hour >=0 && hour < 12) {
@@ -22,6 +22,32 @@ const Page = () => {
   text-white bg-gradient-to-r from-sky-500 to-indigo-500">
       <div className="text-9xl">{fullTime}</div>
       <div className="text-5xl font-bold">{greeting}</div>
+  </div>
+  );
+};
+
+export default Page;
+*/
+
+/** Exercício: Greeting condicional 2 
+ * 
+*/
+  const Page = () => {
+  const fullTime = new Intl.DateTimeFormat('pt-BR',{
+    timeStyle:'short',
+    hour12: false
+  }).format();
+
+  const hour = new Date().getHours();
+ 
+  return (
+  <div className="w-screen h-screen flex flex-col justify-center items-center
+  text-white bg-gradient-to-r from-sky-500 to-indigo-500">
+      <div className="text-9xl">{fullTime}</div>
+      <div className="text-5xl font-bold"></div>
+      {hour >= 0 && hour < 12 && 'Bom Dia 😀'}
+      {hour >= 12 && hour < 18 && 'Boa Tarde 😎'}
+      {hour >= 18 && hour <= 23 && 'Boa Noite 😴'}
   </div>
   );
 };
