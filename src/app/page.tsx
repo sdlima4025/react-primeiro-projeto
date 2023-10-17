c7912dbe3d4aa812db2350ea36318bded5b72dad
sdlima4025 <sergiolimafg3@gmail.com>
2023-10-17 19:47:19 -0300

    mudando state no tempo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -140,7 +140,7 @@ export default Page */
 
 /** O que é um state?
  *
- * */
+ * 
 
 "use client";
 
@@ -161,5 +161,32 @@ const Page = () => {
     </div>
   );
 };
+
+export default Page; 
+*/
+
+/** States mudando no tempo */
+"use client"
+
+import { useState } from "react";
+
+const Page = () => {
+  const [count, setCount] = useState(0);
+
+  const handleBtnClick = () => {
+    setCount(count + 2);
+  }
+  return (
+    <div className="w-screen h-screen flex flex-col justify-center items-center text-3xl">
+      <p> {count} </p>
+      <button
+        onClick={handleBtnClick}
+        className="bg-blue-700 text-white p-3 rounded-md"
+      >
+        +2
+      </button>
+    </div>
+  );
+}
 
 export default Page;
