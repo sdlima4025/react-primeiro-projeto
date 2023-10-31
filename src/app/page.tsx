87cb140070cba920317420dfd4d781934b1f7f3c
sdlima4025 <sergiolimafg3@gmail.com>
2023-10-31 11:16:58 -0300

    useeffect hook

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -47,61 +47,95 @@
 
 /** Exercicio: Quiz */
 
-"use client";
+// "use client";
 
-import { QuestionItem } from "@/components/QuestionItem";
-import { Results } from "@/components/Results";
-import { questions, question } from '@/data/questions';
-import { useState } from "react";
+// import { QuestionItem } from "@/components/QuestionItem";
+// import { Results } from "@/components/Results";
+// import { questions } from "@/data/questions";
+// import { useState } from "react";
+
+// const Page = () => {
+//   const [answers, setAnswers] = useState<number[]>([]);
+//   const [currentQuestion, setcurrentQuestion] = useState(0);
+//   const [showResult, setShowResult] = useState(false);
+
+//   const title = "Quiz Biblico";
+
+//   const loadNextQuestion = () => {
+//     if (questions[currentQuestion + 1]) {
+//       setcurrentQuestion(currentQuestion + 1);
+//     } else {
+//       setShowResult(true);
+//     }
+//   };
+
+//   const handleAnswer = (answer: number) => {
+//     setAnswers([...answers, answer]);
+//     loadNextQuestion();
+//   };
+//   // REINICIANDO O QUIZ
+//   const handleReatarButton = () => {
+//     setAnswers([]);
+//     setcurrentQuestion(0);
+//     setShowResult(false);
+//   };
+
+//   return (
+//     <div className="w-full h-screen flex justify-center items-center bg-blue-600">
+//       <div className="w-full max-w-xl rounded-md bg-white text-black shadow shadow-black">
+//         <div className="p-5 font-bold text-2xl border-b border-gray-300">
+//           {title}
+//         </div>
+//         <div className="p-5">
+//           {!showResult && (
+//             <QuestionItem
+//               question={questions[currentQuestion]}
+//               count={currentQuestion + 1}
+//               onAnswer={handleAnswer}
+//             />
+//           )}
+//           {showResult && <Results questions={questions} answers={answers} />}
+//         </div>
+//         <div className="p-5 text-center border-t border-gray-300">
+//           {!showResult &&
+//             `${currentQuestion + 1} de ${questions.length} pergunta${
+//               questions.length === 1 ? "" : "s"
+//             }`}
+//           {showResult && (
+//             <button
+//               onClick={handleReatarButton}
+//               className="px-2 py-2 rounded-md bg-blue-800 text-white"
+//             >
+//               Reiniciar Quiz
+//             </button>
+          // )}
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Page;
+
+"use client"
+
+import { useEffect } from "react";
 
 const Page = () => {
-  const [answers, setAnswers] = useState<number[]>([]);
-  const [currentQuestion, setcurrentQuestion] = useState(0);
-  const [showResult,setShowResult] = useState(false);
+  /**
+   * 1. Definição da função que vai rodar
+   * 2. Definição de quando vai rodar
+   * 3. Definição do que fazer quando o componente sumir/unload.
+   */
 
-  const title = "Quiz Biblico";
-
-  const loadNextQuestion = () => {
-   if (questions[currentQuestion + 1]) {
-    setcurrentQuestion(currentQuestion + 1);
-   } else {
-    setShowResult(true);
-   }
-  }
-
-  const handleAnswer = (answer: number) => {
-    setAnswers([...answers, answer]);
-    loadNextQuestion();
-   }
-  return (
-    <div className="w-full h-screen flex justify-center items-center bg-blue-600">
-      <div className="w-full max-w-xl rounded-md bg-white text-black shadow shadow-black">
-        <div className="p-5 font-bold text-2xl border-b border-gray-300">
-          {title}
-        </div>
-        <div className="p-5">
-          {!showResult && 
-          <QuestionItem
-            question={questions[currentQuestion]}
-            count={currentQuestion + 1}
-            onAnswer={handleAnswer}
-          />
-        }
-        {showResult && 
-        <Results questions={questions} answers={answers} />
-        }
-        </div>
-        <div className="p-5 text-center border-t border-gray-300">
-          {!showResult && 
-          `${currentQuestion + 1} de ${questions.length} pergunta${questions.length === 1 ? '' : 's'}`
-          }
-          {showResult && 
-            <button className="px-2 py-2 rounded-md bg-blue-800 text-white">Reiniciar Quiz</button>
-          }
-        </div>
-      </div>
+  useEffect(() => {
+    console.log('Rodou o effect...')
+  });
+  return(
+    <div className="">
+      ...
     </div>
   );
-};
+}
 
 export default Page;
--- src/components/Results.tsx
+++ src/components/Results.tsx
@@ -1,5 +1,24 @@
-export const Results = () => {
-    return(
-        <div>...</div>
-    );
+import { Question } from "@/types/Question";
+
+
+type Props = {
+  questions: Question[];
+  answers: number[];
+};
+
+export const Results = ({ questions, answers }: Props) => {
+  return (
+    <div>
+      {questions.map((item, key) => ( 
+        <div key={key} className="mb-3">
+          <div className="font-bold">{key + 1}. {item.questions}</div>
+          <div>
+            <span>({item.answer === answers[key] ? 'Acertou!' : 'Errou!'}) - </span>
+            {item.options[item.answer]}
+          </div>
+        </div>
+
+      ))}
+    </div>
+  )
 }
